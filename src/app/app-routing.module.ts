73860f3449097dbba82ae81dde62f700c3b8b5c2
web-dave <dave@webdave.de>
2018-03-20 22:34:51 +0100

    app routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,30 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { StartComponent } from "./framework/start/start.component";
+import { ImpressumComponent } from "./framework/impressum/impressum.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    redirectTo: "/start",
+    pathMatch: "full"
+  },
+  {
+    path: "start",
+    component: StartComponent
+  },
+  {
+    path: "impressum",
+    component: ImpressumComponent
+  },
+  {
+    path: "blog",
+    loadChildren: "./blog/blog.module#BlogModule"
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
