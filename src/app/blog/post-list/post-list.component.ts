99de08b18c2edb3a9526788569ec9eddde1ea894
web-dave <dave@webdave.de>
2018-05-07 15:53:48 +0200

    npm install -g rxjs-tslint rxjs-5-to-6-migrate -p src/tsconfig.app.json

--- src/app/blog/post-list/post-list.component.ts
+++ src/app/blog/post-list/post-list.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, OnDestroy } from "@angular/core";
 import { BlogsService, post } from "../../framework/shared/blogs.service";
-import { Subscription } from "rxjs/Subscription";
+import { Subscription } from "rxjs";
 
 @Component({
   selector: "blog-post-list",
