e3d9de8273dc222822ad6bf220035ed3fbb6762c
Dave <dave@webdave.de>
2019-02-17 18:56:26 +0100

    click on tile brings you to the post

--- src/app/blog/dashboard/dashboard.component.ts
+++ src/app/blog/dashboard/dashboard.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from "@angular/core";
 import { BreakpointObserver } from "@angular/cdk/layout";
 import { BlogsService, Post } from "../../framework/shared/blogs.service";
 import { Observable } from "rxjs";
+import { Router, ActivatedRoute } from "@angular/router";
 
 @Component({
   selector: "blog-dashboard",
@@ -12,9 +13,14 @@ export class DashboardComponent implements OnInit {
   posts$: Observable<Post[]>;
   constructor(
     private breakpointObserver: BreakpointObserver,
-    private blogService: BlogsService
+    private blogService: BlogsService,
+    private router: Router,
+    private route: ActivatedRoute
   ) {}
   ngOnInit(): void {
     this.posts$ = this.blogService.getPostList();
+  }
+  showMore(name: string) {
+    this.router.navigate([name], { relativeTo: this.route });
   }
 }
