99851eebc87639b6ba4e0a38d4f610a3dc303be9
Dave <dave@webdave.de>
2019-03-21 15:23:14 +0100

    NPM I

--- scripts/getfiles.js
+++ scripts/getfiles.js
@@ -1,10 +1,10 @@
-const directory = "./src/assets/posts/";
-const fs = require("fs");
+const directory = './src/assets/posts/';
+const fs = require('fs');
 const postsArray = [];
 let processed = 0;
 
 const posts = fs.readdirSync(directory);
-let popstCount = posts.filter(name => name.indexOf(".md") !== -1).length;
+let popstCount = posts.filter(name => name.indexOf('.md') !== -1).length;
 posts.sort((a, b) => {
   return (
     fs.statSync(directory + a).mtime.getTime() -
@@ -12,36 +12,42 @@ posts.sort((a, b) => {
   );
 });
 
-posts.filter(name => name.indexOf(".md") !== -1).forEach(post => {
-  let postObj = {
-    url: "",
-    name: "",
-    title: "",
-    preview: ""
-  };
-  postObj.url = `assets/posts/${post}`;
-  postObj.name = post.replace(".md", "");
-
-  fs.readFile(`${directory}${post}`, "utf8", function(err, postcontent) {
-    processed++;
-    if (err) {
-      return console.log(err);
-    }
-    let preview = postcontent.substr(0, 200);
-
-    posts[popstCount - 1];
-
-    postObj.preview = postcontent.substr(0, 200);
-    let title = postcontent
-      .substr(0, postcontent.indexOf("</strong>"))
-      .replace("<strong>", "");
-    postObj.title = title;
-    postsArray.push(postObj);
-    if (popstCount === processed) {
-      writeJson(postsArray);
-    }
-  });
+posts.forEach(p => {
+  console.log(fs.statSync(directory + p).mtime.getTime());
 });
+
+posts
+  .filter(name => name.indexOf('.md') !== -1)
+  .forEach(post => {
+    let postObj = {
+      url: '',
+      name: '',
+      title: '',
+      preview: ''
+    };
+    postObj.url = `assets/posts/${post}`;
+    postObj.name = post.replace('.md', '');
+
+    fs.readFile(`${directory}${post}`, 'utf8', function(err, postcontent) {
+      processed++;
+      if (err) {
+        return console.log(err);
+      }
+      let preview = postcontent.substr(0, 200);
+
+      posts[popstCount - 1];
+
+      postObj.preview = postcontent.substr(0, 200);
+      let title = postcontent
+        .substr(0, postcontent.indexOf('</strong>'))
+        .replace('<strong>', '');
+      postObj.title = title;
+      postsArray.push(postObj);
+      if (popstCount === processed) {
+        writeJson(postsArray);
+      }
+    });
+  });
 
 writeJson = postsarr => {
   const arr = JSON.stringify(postsarr);
