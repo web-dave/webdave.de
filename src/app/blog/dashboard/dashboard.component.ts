4d800225a96026a8f7660fe2ba0c6db10267f897
Dave <dave@webdave.de>
2019-04-01 23:39:32 +0200

    sort the posta by timestamp

--- src/app/blog/dashboard/dashboard.component.ts
+++ src/app/blog/dashboard/dashboard.component.ts
@@ -1,13 +1,14 @@
-import { Component, OnInit } from "@angular/core";
-import { BreakpointObserver } from "@angular/cdk/layout";
-import { BlogsService, Post } from "../../framework/shared/blogs.service";
-import { Observable } from "rxjs";
-import { Router, ActivatedRoute } from "@angular/router";
+import { Component, OnInit } from '@angular/core';
+import { BreakpointObserver } from '@angular/cdk/layout';
+import { BlogsService, Post } from '../../framework/shared/blogs.service';
+import { Observable } from 'rxjs';
+import { Router, ActivatedRoute } from '@angular/router';
+import { tap } from 'rxjs/operators';
 
 @Component({
-  selector: "blog-dashboard",
-  templateUrl: "./dashboard.component.html",
-  styleUrls: ["./dashboard.component.scss"]
+  selector: 'blog-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
   posts$: Observable<Post[]>;
@@ -18,7 +19,14 @@ export class DashboardComponent implements OnInit {
     private route: ActivatedRoute
   ) {}
   ngOnInit(): void {
-    this.posts$ = this.blogService.getPostList();
+    this.posts$ = this.blogService.getPostList().pipe(
+      tap(p => {
+        console.log(p);
+        return p.sort((a, b) =>
+          a.timestamp > b.timestamp ? -1 : b.timestamp > a.timestamp ? 1 : 0
+        );
+      })
+    );
   }
   showMore(name: string) {
     this.router.navigate([name], { relativeTo: this.route });
