6736945b0d50049c87fdbb6a7452487d5d2aeb98
Dave <dave@webdave.de>
2019-10-13 21:38:00 +0200

    updated cropper

--- src/app/cropper/cropper.component.ts
+++ src/app/cropper/cropper.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, ViewChild, ViewChildren } from '@angular/core';
 
 import {
   CropperSettings,
@@ -20,17 +20,20 @@ export class CropperComponent {
   public data2: any;
   public cropperSettings2: CropperSettings;
 
-  @ViewChild('cropper1', { static: false })
-  public cropper1: ImageCropperComponent;
+  @ViewChildren('img-cropper')
+  public cropper: ImageCropperComponent[];
 
-  @ViewChild('cropper2', { static: false })
-  public cropper2: ImageCropperComponent;
+  // @ViewChild('cropper1', { static: true })
+  // public cropper1: ImageCropperComponent;
 
-  @ViewChild('cropper3', { static: false })
-  public cropper3: ImageCropperComponent;
+  // @ViewChild('cropper2', { static: true })
+  // public cropper2: ImageCropperComponent;
 
-  @ViewChild('cropper4', { static: false })
-  public cropper4: ImageCropperComponent;
+  // @ViewChild('cropper3', { static: true })
+  // public cropper3: ImageCropperComponent;
+
+  // @ViewChild('cropper4', { static: true })
+  // public cropper4: ImageCropperComponent;
 
   public onChange: ($event: any) => void;
   public updateCropPosition: () => any;
@@ -70,82 +73,76 @@ export class CropperComponent {
 ></img-cropper>`;
 
   constructor() {
-    this.cropperSettings1 = new CropperSettings();
-    this.cropperSettings1.dynamicSizing = true;
-    this.cropperSettings1.cropperClass = 'custom-class';
-    this.cropperSettings1.croppingClass = 'cropping';
-    this.cropperSettings1.width = 200;
-    this.cropperSettings1.height = 200;
-
-    this.cropperSettings1.croppedWidth = 200;
-    this.cropperSettings1.croppedHeight = 200;
-
-    this.cropperSettings1.canvasWidth = 500;
-    this.cropperSettings1.canvasHeight = 300;
-
-    this.cropperSettings1.minWidth = 100;
-    this.cropperSettings1.minHeight = 100;
-
-    this.cropperSettings1.rounded = false;
-
-    this.cropperSettings1.cropperDrawSettings.strokeColor =
-      'rgba(255,255,255,1)';
-    this.cropperSettings1.cropperDrawSettings.strokeWidth = 2;
-
-    this.cropperSettings1.keepAspect = true;
-    this.cropperSettings1.preserveSize = false;
+    const settings1 = {
+      dynamicSizing: true,
+      cropperClass: 'custom-class',
+      croppingClass: 'cropping',
+      width: 200,
+      height: 200,
+      croppedWidth: 200,
+      croppedHeight: 200,
+      canvasWidth: 500,
+      canvasHeight: 300,
+      minWidth: 100,
+      minHeight: 100,
+      rounded: false,
+      cropperDrawSettings: {
+        strokeColor: '#fff',
+        trokeWidth: 2
+      },
+      keepAspect: true,
+      preserveSize: false
+    };
+    this.cropperSettings1 = new CropperSettings(settings1);
 
     this.data1 = {};
 
     // Cropper settings 2
-    this.cropperSettings2 = new CropperSettings();
-    this.cropperSettings2.width = 200;
-    this.cropperSettings2.height = 200;
-    this.cropperSettings2.keepAspect = false;
-
-    this.cropperSettings2.croppedWidth = 200;
-    this.cropperSettings2.croppedHeight = 200;
-
-    this.cropperSettings2.canvasWidth = 500;
-    this.cropperSettings2.canvasHeight = 300;
-
-    this.cropperSettings2.minWidth = 100;
-    this.cropperSettings2.minHeight = 100;
-
-    this.cropperSettings2.rounded = true;
-    this.cropperSettings2.minWithRelativeToResolution = false;
-
-    this.cropperSettings2.cropperDrawSettings.strokeColor =
-      'rgba(255,255,255,1)';
-    this.cropperSettings2.cropperDrawSettings.strokeWidth = 2;
-    this.cropperSettings2.noFileInput = true;
-    this.cropperSettings2.fileType = 'image/jpeg';
+    const settings2 = {
+      width: 200,
+      height: 200,
+      keepAspect: false,
+      croppedWidth: 200,
+      croppedHeight: 200,
+      canvasWidth: 500,
+      canvasHeight: 300,
+      minWidth: 100,
+      minHeight: 100,
+      rounded: true,
+      minWithRelativeToResolution: false,
+      cropperDrawSettings: {
+        strokeColor: '#fff',
+        strokeWidth: 2
+      },
+      noFileInput: true,
+      fileType: 'image/jpeg'
+    };
+    this.cropperSettings2 = new CropperSettings(settings2);
 
     this.data2 = {};
 
     // Cropper settings 3
-    this.cropperSettings3 = new CropperSettings();
-    this.cropperSettings3.width = 200;
-    this.cropperSettings3.height = 250;
-    this.cropperSettings3.keepAspect = true;
+    const settings3 = {
+      width: 200,
+      height: 250,
+      keepAspect: true,
+      croppedWidth: 200,
+      croppedHeight: 250,
+      canvasWidth: 500,
+      canvasHeight: 300,
+      minWidth: 100,
+      minHeight: 100,
+      rounded: false,
+      preserveSize: true,
+      minWithRelativeToResolution: false,
+      cropperDrawSettings: {
+        strokeColor: '#fff',
+        strokeWidth: 2
+      },
+      noFileInput: false
+    };
 
-    this.cropperSettings3.croppedWidth = 200;
-    this.cropperSettings3.croppedHeight = 250;
-
-    this.cropperSettings3.canvasWidth = 500;
-    this.cropperSettings3.canvasHeight = 300;
-
-    this.cropperSettings3.minWidth = 100;
-    this.cropperSettings3.minHeight = 100;
-
-    this.cropperSettings3.rounded = false;
-    this.cropperSettings3.preserveSize = true;
-    this.cropperSettings3.minWithRelativeToResolution = false;
-
-    this.cropperSettings3.cropperDrawSettings.strokeColor =
-      'rgba(255,255,255,1)';
-    this.cropperSettings3.cropperDrawSettings.strokeWidth = 2;
-    this.cropperSettings3.noFileInput = false;
+    this.cropperSettings3 = new CropperSettings(settings3);
 
     this.cropperSettings3.resampleFn = (buffer: HTMLCanvasElement) => {
       const canvasContext = buffer.getContext('2d');
@@ -187,33 +184,32 @@ export class CropperComponent {
     this.data3 = {};
 
     // Cropper settings 4
-    this.cropperSettings4 = new CropperSettings();
-    this.cropperSettings4.width = 200;
-    this.cropperSettings4.height = 200;
+    const settings4 = {
+      width: 200,
+      height: 200,
+      croppedWidth: 200,
+      croppedHeight: 200,
+      canvasWidth: 500,
+      canvasHeight: 300,
+      minWidth: 100,
+      minHeight: 100,
+      rounded: false,
+      cropperDrawSettings: {
+        strokeColor: '#fff',
+        strokeWidth: 2
+      },
+      keepAspect: true,
+      preserveSize: true,
+      cropOnResize: false
+    };
 
-    this.cropperSettings4.croppedWidth = 200;
-    this.cropperSettings4.croppedHeight = 200;
-
-    this.cropperSettings4.canvasWidth = 500;
-    this.cropperSettings4.canvasHeight = 300;
-
-    this.cropperSettings4.minWidth = 100;
-    this.cropperSettings4.minHeight = 100;
-
-    this.cropperSettings4.rounded = false;
-
-    this.cropperSettings4.cropperDrawSettings.strokeColor =
-      'rgba(255,255,255,1)';
-    this.cropperSettings4.cropperDrawSettings.strokeWidth = 2;
-
-    this.cropperSettings4.keepAspect = true;
-    this.cropperSettings4.preserveSize = true;
-    this.cropperSettings4.cropOnResize = false;
+    this.cropperSettings4 = new CropperSettings(settings4);
 
     this.data4 = {};
 
     this.getImage = () => {
-      this.data4.image = this.cropper4.cropper.getCroppedImage(true).src;
+      // this.data4.image = this.cropper4.cropper.getCroppedImage(true).src;
+      this.data4.image = this.cropper[3].cropper.getCroppedImage(true).src;
     };
 
     this.onChange = ($event: any) => {
@@ -222,7 +218,8 @@ export class CropperComponent {
       const myReader: FileReader = new FileReader();
       myReader.addEventListener('loadend', (loadEvent: any) => {
         image.src = loadEvent.target.result;
-        this.cropper2.setImage(image);
+        // this.cropper2.setImage(image);
+        this.cropper[1].setImage(image);
       });
 
       myReader.readAsDataURL(file);
@@ -238,10 +235,11 @@ export class CropperComponent {
     };
 
     this.resetCroppers = () => {
-      this.cropper1.reset();
-      this.cropper2.reset();
-      this.cropper3.reset();
-      this.cropper4.reset();
+      this.cropper.forEach(c => c.reset());
+      // this.cropper1.reset();
+      // this.cropper2.reset();
+      // this.cropper3.reset();
+      // this.cropper4.reset();
     };
   }
 }
