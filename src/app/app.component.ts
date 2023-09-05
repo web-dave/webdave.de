dc080ac542f41f2fc7ab9eaa696429719231e270
Dave <dave@webdave.de>
2023-09-05 13:18:23 +0200

    first version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,41 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { timer, fromEvent } from 'rxjs';
+import { content, text } from './app.config';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'webdave_de';
+  tick$ = timer(1000, 80);
+  body = document.querySelector('body') as HTMLBodyElement;
+  e$ = fromEvent(this.body, 'keyUp');
+  line = 0;
+  optionsOffset = content.length;
+
+  lines = text.map((_) => '');
+
+  text = [...text.join('|').split('')];
+
+  ngOnInit() {
+    console.log(text);
+
+    this.tick$.subscribe((data) => {
+      const char = this.text[data];
+      if (char) {
+        if (char === '|') {
+          this.line++;
+        } else {
+          this.lines[this.line] += char;
+        }
+      } else {
+        this.line++;
+      }
+    });
+  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,5 +4,25 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes) ]
+  providers: [provideRouter(routes)],
 };
+
+export const content = [
+  'Welcome at webdave_de',
+  'My content:',
+  '[livestream] https://webdave.tv',
+  '[Blog] https://blog.webdave.de',
+  'Need to boost your skills? Reach out to me.',
+  'I offer:',
+];
+
+export const options = [
+  '[Workshop] Javascript fundamentals',
+  '[Workshop] Angular fundamentals',
+  '[Workshop] Angular advanced',
+  '[Workshop] Testing',
+  '[Workshop] Other topics (RxJS, Redux)',
+  'Consulting',
+];
+
+export const text = [...content, ...options];
